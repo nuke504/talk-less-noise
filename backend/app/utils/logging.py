a7176bacd15ba64ad891fa63fc75841797bd7f93
lohzy <nuke504@gmail.com>
2025-06-15 22:40:17 +0800

    change to sys.stdout for logging in prod

--- backend/app/utils/logging.py
+++ backend/app/utils/logging.py
@@ -1,21 +1,31 @@
 import logging
 import os
 import time
+import sys
 
 from autologging import TRACE
+from ..config.settings import DEPLOYMENT_ENV
 
 
 def init_logging(log_dir: str):
 
-    if not os.path.exists(log_dir):
-        os.makedirs(log_dir)
+    LOG_FORMAT = "%(asctime)s - %(levelname)s : %(name)s : %(funcName)s : %(message)s"
 
-    logging.basicConfig(
-        level=TRACE,
-        filename=os.path.join(
-            log_dir, f'api-{time.strftime("%d-%m-%Y--%H-%M-%S")}.log'
-        ),
-        encoding="utf-8",
-        format="%(asctime)s - %(levelname)s : %(name)s : %(funcName)s : %(message)s",
-    )
+    if DEPLOYMENT_ENV == "production":
+        logging.basicConfig(
+            level=TRACE,
+            stream=sys.stdout,
+            format=LOG_FORMAT,
+        )
+    else:
+        if not os.path.exists(log_dir):
+            os.makedirs(log_dir)
 
+        logging.basicConfig(
+            level=TRACE,
+            filename=os.path.join(
+                log_dir, f'api-{time.strftime("%d-%m-%Y--%H-%M-%S")}.log'
+            ),
+            encoding="utf-8",
+            format=LOG_FORMAT,
+        )
