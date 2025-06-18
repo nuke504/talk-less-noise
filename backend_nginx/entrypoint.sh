f92daccb4c00aae931bdd94292b289e309bbc66f
lohzy <nuke504@gmail.com>
2025-06-19 00:26:52 +0800

    fix bug

--- backend_nginx/entrypoint.sh
+++ backend_nginx/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 echo "BACKEND_API_URL=$BACKEND_API_URL"
 envsubst < /etc/nginx/conf.d/default-bridge.conf.template > /etc/nginx/conf.d/default.conf
+echo "----- /etc/nginx/conf.d/default.conf -----"
+cat /etc/nginx/conf.d/default.conf
+echo "------------------------------------------"
 exec nginx -g 'daemon off;'
