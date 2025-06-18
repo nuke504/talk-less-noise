d29515a9aab61149b1ef35b44b668f5af2702af5
lohzy <nuke504@gmail.com>
2025-06-19 00:00:29 +0800

    fix bugs

--- backend_nginx/entrypoint.sh
+++ backend_nginx/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-envsubst < /etc/nginx/conf.d/default-azure.conf.template > /etc/nginx/conf.d/default.conf
+envsubst < /etc/nginx/conf.d/default-bridge.conf.template > /etc/nginx/conf.d/default.conf
 exec nginx -g 'daemon off;'
