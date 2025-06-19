6561fd8643a52e789a43cf37e3337a1eee6e1070
lohzy <nuke504@gmail.com>
2025-06-19 20:32:14 +0800

    add curl exit code

--- backend_nginx/entrypoint.sh
+++ backend_nginx/entrypoint.sh
@@ -4,5 +4,6 @@ echo "BACKEND_API_URL=$BACKEND_API_URL"
 echo "----- /etc/nginx/conf.d/default.conf -----"
 cat /etc/nginx/conf.d/default.conf
 echo "------------------------------------------"
-curl -v http://backend-api.internal.tln-cae.southeastasia.azurecontainerapps.io:8080
+curl -vf http://backend-api.internal.tln-cae.southeastasia.azurecontainerapps.io:8080/docs
+echo "curl exit code: $?"
 exec nginx -g 'daemon off;'
