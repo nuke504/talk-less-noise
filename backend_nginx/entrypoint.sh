2e1621640561f1c7922f04798670e94bd6731c5f
lohzy <nuke504@gmail.com>
2025-06-19 21:00:17 +0800

    change curl address

--- backend_nginx/entrypoint.sh
+++ backend_nginx/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 echo "BACKEND_API_URL=$BACKEND_API_URL"
 # envsubst < /etc/nginx/conf.d/default-bridge.conf.template > /etc/nginx/conf.d/default.conf
-echo "----- /etc/nginx/conf.d/default.conf -----"
-cat /etc/nginx/conf.d/default.conf
-echo "------------------------------------------"
-curl -vf http://backend-api.internal.tln-cae.southeastasia.azurecontainerapps.io:8080/docs
+# echo "----- /etc/nginx/conf.d/default.conf -----"
+# cat /etc/nginx/conf.d/default.conf
+# echo "------------------------------------------"
+curl -vf http://backend-api.internal.orangesky-fc92084d.southeastasia.azurecontainerapps.io:8080
 echo "curl exit code: $?"
 exec nginx -g 'daemon off;'
