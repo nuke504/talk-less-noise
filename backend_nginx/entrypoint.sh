a55328915b43f6394748577f84b4aaebcb38c0b3
lohzy <nuke504@gmail.com>
2025-06-19 20:19:06 +0800

    test curl

--- backend_nginx/entrypoint.sh
+++ backend_nginx/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 echo "BACKEND_API_URL=$BACKEND_API_URL"
-envsubst < /etc/nginx/conf.d/default-bridge.conf.template > /etc/nginx/conf.d/default.conf
+# envsubst < /etc/nginx/conf.d/default-bridge.conf.template > /etc/nginx/conf.d/default.conf
 echo "----- /etc/nginx/conf.d/default.conf -----"
 cat /etc/nginx/conf.d/default.conf
 echo "------------------------------------------"
+curl -v http://backend-api.internal.tln-cae.southeastasia.azurecontainerapps.io:8080
 exec nginx -g 'daemon off;'
