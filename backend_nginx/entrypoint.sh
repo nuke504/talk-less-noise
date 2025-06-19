fdf5e131362aed938c65bb3572b43fc3fda95009
lohzy <nuke504@gmail.com>
2025-06-19 21:49:43 +0800

    updated config file

--- backend_nginx/entrypoint.sh
+++ backend_nginx/entrypoint.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 echo "BACKEND_API_URL=$BACKEND_API_URL"
-# envsubst < /etc/nginx/conf.d/default-bridge.conf.template > /etc/nginx/conf.d/default.conf
-# echo "----- /etc/nginx/conf.d/default.conf -----"
-# cat /etc/nginx/conf.d/default.conf
-# echo "------------------------------------------"
-curl -v http://backend-api.internal.orangesky-fc92084d.southeastasia.azurecontainerapps.io:8080
+envsubst '$BACKEND_API_URL' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 exec nginx -g 'daemon off;'
