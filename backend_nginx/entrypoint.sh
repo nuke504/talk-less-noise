b45f51b5ec81a00b70aa459a720eafb35662035b
lohzy <nuke504@gmail.com>
2025-06-20 20:37:29 +0800

    add index homepage for nginx

--- backend_nginx/entrypoint.sh
+++ backend_nginx/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "BACKEND_API_URL=$BACKEND_API_URL"
-envsubst '$BACKEND_API_URL' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '$BACKEND_API_URL' < /etc/nginx/conf.d/default-bridge.conf.template > /etc/nginx/conf.d/default.conf
 exec nginx -g 'daemon off;'
